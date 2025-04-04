4892bd977238016a23fbb7bfd5b93a3173f5e306
Rafal Jasinski <rajs@softwaremind.com>
2025-04-04 13:12:21 +0200

    New baseline for a devcontainer

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Check if the user has provided an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <service_to_build>"
-  exit 1
-fi
-
 # Set the environment variables
 export USER_UID=$(id -u)
 export USER_GID=$(id -g)
 
 # Run the docker compose build command with the user-provided argument
-docker compose build "$1"
+docker compose build nvim-devcontainer
 
