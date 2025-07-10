23c07b0f5af773799511e953b3a552b8196c45bc
Rafal Jasinski <rajs@softwaremind.com>
2025-07-10 10:04:31 +0200

    New ways to work with devcontainer

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Set the environment variables
 export USER_UID=$(id -u)
 export USER_GID=$(id -g)
 
-# Run the docker compose build command with the user-provided argument
 docker compose build base-image && \
 docker compose build devcontainer
 
