f4cc9a69815f6c5fc6179a5d4ec361a139e4e7c6
Rafal Jasinski <rajs@softwaremind.com>
2025-07-10 12:54:45 +0200

    Added standalone devcontainer

--- debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export USER_UID=$(id -u)
-export USER_GID=$(id -g)
-
-docker compose run --rm debug
--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 export USER_UID=$(id -u)
 export USER_GID=$(id -g)
 
-docker compose build base-image && \
 docker compose build devcontainer
 
 
